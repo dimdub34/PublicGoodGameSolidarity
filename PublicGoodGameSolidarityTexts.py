c757ff5503578ad626fbd05c1c60347f2f56c4e8
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2016-02-26 10:09:39 +0100

    Texts

--- PublicGoodGameSolidarityTexts.py
+++ PublicGoodGameSolidarityTexts.py
@@ -38,15 +38,41 @@ def get_text_sinistred(sinistred):
 
 
 def get_text_vote():
-    txt = trans_PGGS(u"Text vote")
+    txt = trans_PGGS(u"You must vote in favour of or againts the share of the "
+                     u"public account of your group with a sinistred group.")
     return txt
 
 
 def get_text_explanation():
-    txt = u"Explanation text"
+    txt = trans_PGGS(u"You must choose how much tokens you put in the public "
+                     u"account.")
     return txt
 
 
 def get_text_summary(period_content):
-    txt = u"Summary text"
+    txt = trans_PGGS(u"You put") + \
+          u" {} ".format(get_pluriel(period_content.get("PGGS_indivaccount"),
+                                     trans_PGGS(u"token"))) + \
+        trans_PGGS(u"in your individual account and ") + \
+        u" {} ".format(get_pluriel(period_content.get("PGGS_groupaccount"),
+                                   trans_PGGS(u"token"))) + \
+        trans_PGGS(u"in the public account.") + u"<br />" + \
+        trans_PGGS(u"Your group put a total of") + \
+        u" {} ".format(get_pluriel(period_content.get("PGGS_groupaccountsum"),
+                                   trans_PGGS(u"token"))) + \
+        trans_PGGS(u"in the public account.") + u"<br />" + \
+        trans_PGGS(u"Your payoff for the period is equal to") + \
+        u"{} + {} = {}.".format(period_content.get("PGGS_indivaccountpayoff"),
+                               period_content.get("PGGS_groupaccountpayoff"),
+                               get_pluriel(period_content.get("PGGS_periodpayoff"),
+                                           pms.MONNAIE))
+
+    if pms.TREATMENT == pms.get_treatment("sol_auto") or \
+            (pms.TREATMENT == pms.get_treatment("sol_vote") and
+                     period_content.get("PGGS_votemajority") == pms.IN_FAVOR):
+        txt += u"<br />" + trans_PGGS(u"Each member of the sinistred "
+                                      u"groups has a payoff for the period "
+                                      u"equal to") + u" {}.".format(
+            get_pluriel(period_content.get("PGGS_groupaccountpayoff"),
+                        pms.MONNAIE))
     return txt

589ca13d5666ad952c9cca3bdaafdaca74cb1525
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2017-03-21 11:53:29 +0100

    add payoff from expectations

--- PublicGoodGameSolidarityPart.py
+++ PublicGoodGameSolidarityPart.py
@@ -247,6 +247,12 @@ class PartiePGGS(Partie):
         self.PGGS_gain_ecus = self.currentperiod.PGGS_cumulativepayoff
         self.PGGS_gain_euros = float("{:.2f}".format(
             float(self.PGGS_gain_ecus) * float(pms.TAUX_CONVERSION)))
+        # ajout des expectations
+        expectations_payoffs = sum([p.PGGS_expectation_payoff for p in
+                                    self.repetitions])
+        self.joueur.info(u"PP {}, EP {}".format(
+            self.PGGS_gain_euros, expectations_payoffs))
+        self.PGGS_gain_euros += expectations_payoffs
         yield (self.remote.callRemote(
             "set_payoffs", self.PGGS_gain_euros, self.PGGS_gain_ecus))
 
@@ -387,6 +393,7 @@ class RepetitionsPGGS(Base):
         self.PGGS_indivaccountpayoff = 0
         self.PGGS_groupaccountpayoff = 0
         self.PGGS_groupaccountsharedpayoff = 0
+        self.PGGS_expectation_payoff = 0
         self.PGGS_periodpayoff = 0
         self.PGGS_cumulativepayoff = 0
 
